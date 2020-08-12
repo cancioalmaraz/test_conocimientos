2d592ab11599502fafd0b472355f10454d6d5343
Cancio <cancioalmaraz@gmail.com>
2020-08-12 00:55:00 -0400

    Mas arreglos en el responsive

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,10 +1,22 @@
-<nav class="navbar navbar-expand-lg navbar navbar-dark flex-md-row bd-navbar" style="background-color: #732077;">
-	<a class="navbar-brand" style="font-size: 30px">Test De Conocimientos</a>
+<style>
+	.navbar-collapse{
+		text-align: center;
+	}
+	.navbar-brand{
+		font-size: 30px;
+	}
+
+
+
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #732077;">
+	<a class="navbar-brand">Test De Conocimientos</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarNav">
-		<ul class="navbar-nav">
+		<ul class="navbar-nav mr-auto">
 			<li class="nav-item {{ setActive('home') }}">
 				<a class="nav-link" href="/" style="font-size: 25px">Problemas <span class="sr-only">(current)</span></a>
 			</li>
