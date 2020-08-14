959ed7c0a2ccb0ad7ff702d48a4a5951b0f4d4dc
Cancio <cancioalmaraz@gmail.com>
2020-08-13 23:03:32 -0400

    Anadimos boton para cargar ejemplo

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+function pegarEjemplo1(id_element) {
+
+	document.getElementById(id_element).value ="Yendo un weekend a lo de Damian,\r\n\
+Tenia urgencia de hablar con el man,\r\n\
+Camine porque pinche mi van,\r\n\
+Vi una mina de la que soy fan\r\n\
+\r\n\
+Una que sale por el canal Sony\r\n\
+En una serie que esta con un pony\r\n\
+Y en mi casa del barrio Marconi\r\n\
+Siempre la veo tomandome un Johnny";
+
+}
+function pegarEjemplo2(id_element) {
+
+	document.getElementById(id_element).value = "Historial ayer borrado\r\n\
+anteayer hubo pecado\r\n\
+\r\n\
+Esto no pega\r\n\
+ni con cola.\r\n\
+\r\n\
+Era un simple clerigo, pobre de clerecia,\r\n\
+dicie cutiano missa de la sancta Maria;\r\n\
+non sabie decir otra, diciela cada dia,\r\n\
+mas la sabie por uso qe por sabiduria.\r\n\
+\r\n\
+Un manotazo duro, un golpe helado,\r\n\
+un hachazo invisible y homicida,\r\n\
+un empujon brutal te ha derribado.";
+
+}
--- resources/views/partials/textAreaEntrada.blade.php
+++ resources/views/partials/textAreaEntrada.blade.php
@@ -1,12 +1,17 @@
 <form method="GET" action="/problems/{{ $id }}">
 	@csrf
 	<div class="form-group" style="margin-left: 30px; margin-right: 30px;">
-		<label for="textProblem" style="font-size: 25px;"><strong>Ingrese Las Frases</strong></label>
+			<label for="textProblem" style="font-size: 25px;"><strong>Ingrese Las Frases</strong></label>
 		<textarea name="text" class="form-control" id="textProblem" rows="10">{{ $older }}</textarea>
 		<br>
 		<div>
 			<button type="submit" class="btn btn-primary mb-2">Ejecutar</button>
 			<a href="/problems/{{ $id }}" class="btn btn-primary mb-2" style="margin-left: 10px;">Limpiar</a>
+			@if ($id == 1)
+				<button class="btn btn-primary mb-2" onclick="pegarEjemplo1('textProblem')" style="margin-left: 10px;">Pegar Ejemplo</button>
+			@else
+				<button class="btn btn-primary mb-2" onclick="pegarEjemplo2('textProblem')" style="margin-left: 10px;">Pegar Ejemplo</button>
+			@endif
 		</div>
 	</div>
 </form>
